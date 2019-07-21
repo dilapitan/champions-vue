39f949fb8d00693886c064b9e258703b3ceb67ab
dilapitan <domlapitan@gmail.com>
2019-07-21 19:36:12 +0800

    Finish NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="ui inverted menu">
-    <a class="item">
+    <a 
+      href="https://vuejs.org/"
+      class="item"
+      target="_blank"
+    >
       <img src="@/assets/logo.png" >
     </a>
-    <a class="item" >CHAMPIONS-VUE</a>
+    <a class="item">
+      <span>CHAMPIONS</span>
+      <span id="vue-header">VUE</span>
+    </a>
+    <a  
+      href="https://github.com/dilapitan/champions-vue"
+      class="item right floated"
+      target="_blank"
+    >
+      <i class="large icons green github icon"></i>
+    </a>
   </div>
 </template>
 
@@ -12,3 +26,10 @@ export default {
   name: 'NavBar'
 }
 </script>
+
+style <style scoped>
+  #vue-header {
+    color: #42b883;
+    font-weight: 700;
+  }
+</style>
