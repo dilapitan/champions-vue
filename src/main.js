814fb4299b26f95555e7eef031d01bb4c82b4fcd
dilapitan <domlapitan@gmail.com>
2019-07-21 18:13:12 +0800

    Add semantic-ui-css package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+window.$ = window.jQuery = require('jquery')
+
+require('semantic-ui-css/semantic.css')
+require('semantic-ui-css/semantic.js')
 
 Vue.config.productionTip = false
 
